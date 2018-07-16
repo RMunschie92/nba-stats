c24e062a51ddbf563db397a9a7060abbecb502e4
Ryan Munsch <rmunsch9632@gmail.com>
2018-07-16 16:04:50 -0400

    team routing added

--- routes/api/player.js
+++ routes/api/player.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const request = require('request');
 
-const teamData = require("../../data/teamList.js");
+const teamList = require("../../data/teamList.js");
 
 const password = 'MYSPORTSFEEDS';
 const apiKey = "73a08190-f41f-4bda-becd-af5c5a";
@@ -25,44 +25,42 @@ router.get("/:name", (req, res) => {
         "Authorization": auth
       }
     })
-      .on("error", err => {
-        console.error('Error: ', err.message);
-      })
-      .on("response", response => {
-        console.log("statusCode: ", response.statusCode);
-      })
-      .on("data", chunk => {
-        body += chunk
-      })
-      .on("end", () => {
-        body = JSON.parse(body);
-        responses.push(body);
-        completedRequests++;
-        // when both requests are completed
-        if (completedRequests == urls.length) {
-          // reverse responses array if playerData is 0 index
-          if (responses[0].hasOwnProperty('leaguePlayers')) {
-            responses = responses.reverse();
-          }
-          let playerStats = responses[0].seasonalPlayerStats.playerStatsTotals[0].stats;
-          let playerData = responses[1].leaguePlayers.players[0].player;
-          // console.log(playerStats);
-          // console.log(playerData);
-          // get team name to send to view
-          teamData.map(team => {
-            if (team.abbreviation == playerData.currentTeam.abbreviation.toLowerCase()) {
-              teamName = team.city + ' ' + team.name;
-              teamLogo = team.image;
-            }
-          })
-          return res.render("player", {
-            playerStats: playerStats,
-            playerData: playerData,
-            teamName: teamName,
-            teamLogo: teamLogo
-          });
+    .on("error", err => {
+      console.error('Error: ', err.message);
+    })
+    .on("response", response => {
+      console.log("statusCode: ", response.statusCode);
+    })
+    .on("data", chunk => {
+      body += chunk
+    })
+    .on("end", () => {
+      body = JSON.parse(body);
+      responses.push(body);
+      completedRequests++;
+      // when both requests are completed
+      if (completedRequests == urls.length) {
+        // reverse responses array if playerData is 0 index
+        if (responses[0].hasOwnProperty('players')) {
+          responses = responses.reverse();
         }
-      })
+        let playerStats = responses[0].playerStatsTotals[0].stats;
+        let playerData = responses[1].players[0].player;
+        // get team name to send to view
+        teamList.map(team => {
+          if (team.abbreviation == playerData.currentTeam.abbreviation.toLowerCase()) {
+            teamName = team.city + ' ' + team.name;
+            teamLogo = team.image;
+          }
+        })
+        return res.render("player", {
+          playerStats: playerStats,
+          playerData: playerData,
+          teamName: teamName,
+          teamLogo: teamLogo
+        });
+      }
+    })
   })
 });
 
--- routes/api/team.js
+++ routes/api/team.js
@@ -1,26 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const request = require("request");
-const https = require("https");
 const moment = require("moment");
+
+const teamList = require("../../data/teamList.js");
 
 const date = moment().format('YYYY/MM/DD').replace(/\//g,'');
 
-// Initialize array to store data in through handler 
-let templateData = [];
-
-const username = "ryan_munsch";
 const password = "MYSPORTSFEEDS";
 const apiKey = "73a08190-f41f-4bda-becd-af5c5a";
 
-router.get('/', (req, res) => {
-
-  let body = "";
-  let url = `https://api.mysportsfeeds.com/v2.0/pull/nba/players.json?date=${date}&team=bos`;
+router.get('/:teamName', (req, res) => {
+  let urls = [
+    `https://api.mysportsfeeds.com/v2.0/pull/nba/players.json?date=${date}&team=${req.params.teamName}`, 
+    `https://api.mysportsfeeds.com/v2.0/pull/nba/2017-2018-regular/team_stats_totals.json?team=${req.params.teamName}`
+    // `https://api.mysportsfeeds.com/v2.0/pull/nba/2017-2018-regular/standings.json`
+  ];
   let auth = "Basic " + Buffer.from(apiKey + ":" + password).toString("base64");
+  let completedRequests = 0;
+  let responses = [];
+  let roster = [];
+  let teamName;
 
-  request
-    .get({ url: url, headers: { Authorization: auth } })
+  urls.forEach((url) => {
+    let body = "";
+    request.get({
+      url: url,
+      headers: { 
+        "Authorization": auth 
+      }
+    })
     .on("error", err => {
       console.error("Error: ", err.message);
     })
@@ -31,18 +40,46 @@ router.get('/', (req, res) => {
       body += chunk;
     })
     .on("end", () => {
-      const data = JSON.parse(body);
-      let rawData = data.leaguePlayers.players;
-      if (templateData.length < 1) {
-        rawData.map(player => {
-          if (player.player.currentRosterStatus === "ROSTER") {
-            templateData.push(player.player);
+      body = JSON.parse(body);
+      responses.push(body);
+      completedRequests++;
+
+      // when both requests are complete
+      if (completedRequests == urls.length) {
+
+        console.log(responses);
+
+        // reverse responses array if players is 0 index
+
+        if (responses[0].hasOwnProperty('players')) {
+          responses = responses.reverse();
+        }
+        let teamStats = responses[0].teamStatsTotals[0].stats;
+        console.log(teamStats)
+        let rawData = responses[1].players;
+
+        // set teamData
+        teamList.map(team => {
+          if (team.abbreviation == req.params.teamName.toLowerCase()) {
+            teamData = team;
           }
         });
-      }
-      return res.render("team", { data: templateData });
+
+        // push each player object into templateData if it has a jerseyNumber
+        rawData.map(player => {
+          if (player.player.jerseyNumber !== null) {
+            roster.push(player.player);
+          }
+        });
+
+        return res.render("team", {
+          roster: roster,
+          teamStats: teamStats,
+          team: teamData
+        });
+      }      
     });
-  
+  })
 })
 
 module.exports = router;
